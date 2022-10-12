8475e2d1d813f425804a71174f7cb32d513cb505
Hold Jhon <jhonmc117@gmail.com>
2022-10-12 10:59:17 -0500

    Conexion con el servlet

--- src/main/java/com/molano/formulariocomp/control.java
+++ src/main/java/com/molano/formulariocomp/control.java
@@ -27,12 +27,12 @@ public class control extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
+        try ( PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet Control</title>");            
+            out.println("<title>Servlet Control</title>");
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet control at " + request.getContextPath() + "</h1>");
@@ -53,7 +53,26 @@ public class control extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        response.setContentType("text/html;charset=UTF-8");
+
+        String nombres = request.getParameter("txt_nombres");
+        String apellidos = request.getParameter("txt_apellidos");
+        String correo = request.getParameter("txt_correo");
+        String numero = request.getParameter("txt_telefono");
+
+        try ( PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Registro</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Bienvenido: " + nombres + " " + apellidos + "</h1>");
+            out.println("<h1>Correo: " + correo + "</h1>");
+            out.println("<h1>Telefono: " + numero + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
     }
 
     /**
@@ -67,24 +86,7 @@ public class control extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String nombres = request.getParameter("txt_nombres");
-        String apellidos = request.getParameter("txt_apellidos");
-        String correo = request.getParameter("txt_correo");
-        String numero = request.getParameter("txt_numero");
-        try (PrintWriter out = response.getWriter()) {
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Control</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet control at " + request.getContextPath() + "</h1>");
-            out.println("<h1>Bienvenido: " + nombres + " " + apellidos + "</h1>");
-            out.println("<h1>Correo: " + correo + "</h1>");
-            out.println("<h1>Telefono: " + numero + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        processRequest(request, response);
     }
 
     /**
--- target/FormularioComp-1.0-SNAPSHOT/index.jsp
+++ target/FormularioComp-1.0-SNAPSHOT/index.jsp
@@ -1,26 +1,23 @@
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
-
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <link rel="stylesheet" href="./style/css.css">
-    <title>Parcial Jhon Molano</title>
-</head>
-
-<body>
-    <form>
-        <div class="principal">
-            <h2>Registro</h2>
-            <h4>Ingresa los datos</h4>
-            <input type="text" name="txt_nombres" placeholder="Nombre">
-            <input type="text" name="txt_apellidos" placeholder="Apellido">
-            <input type="email" name="txt_correo" placeholder="Correo">
-            <input type="number" name="txy_telefono" placeholder="Telefono">
-            <input type="password" name="txt_contrasena" placeholder="Contraseña">
-            <button type="submit" name="btnRegistrar">REGISTRAR</button>
-        </div>
-    </form>
-</body>
-
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="./style/css.css">
+        <title>Parcial Jhon Molano</title>
+    </head>
+    <body>
+        <form action="control" method="get">
+            <div class="principal">
+                <h2>Registro</h2>
+                <h4>Ingresa los datos</h4>
+                <input type="text" name="txt_nombres" placeholder="Nombre" required>
+                <input type="text" name="txt_apellidos" placeholder="Apellido" required>
+                <input type="email" name="txt_correo" placeholder="Correo" required>
+                <input type="text" name="txt_telefono" placeholder="Telefono" required>
+                <input type="password" name="txt_contrasena" placeholder="Contraseña" required>
+                <button type="submit" name="btnRegistrar" value="Registrar">Registrar</button>
+            </div>
+        </form>
+    </body>
 </html>
