108a96e7e88853b2cee8b660ac7e1d453ad6b867
Jhon <jhonmc117@gmail.com>
2022-10-20 08:09:30 -0500

    Singleton

--- src/main/java/com/molano/formulariocomp/PersonaDAO.java
+++ src/main/java/com/molano/formulariocomp/PersonaDAO.java
@@ -1,9 +1,36 @@
-package com.molano.formulariocomp;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author Jhon
  */
-public class PersonaDAO {
-    
+public class PersonaDAO{
+    private static final String SQL_READALL="select * from usuario ";
+    private static final ConexionBD con = new ConexionBD();
+    public PersonaDAO() {
+        con.conectar();
+    }
+    public List<PersonaDTO> readAll(){
+        List<PersonaDTO> lista = null;
+        PreparedStatement ps;
+        try {
+            ps = con.getConexion().prepareStatement(SQL_READALL);
+            ResultSet rs = ps.executeQuery();
+            lista = new ArrayList<>();
+            while (rs.next()){
+                PersonaDTO obj = new PersonaDTO(rs.getString("nombre"),
+                        rs.getString("apellido"),
+                        rs.getString("correo"),
+                        rs.getString("clave"),
+                lista.add(obj);
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error al conectar: "+ ex);
+        }
+        return lista;
+    }
 }
--- src/main/java/com/molano/formulariocomp/PersonaDTO.java
+++ src/main/java/com/molano/formulariocomp/PersonaDTO.java
@@ -1,9 +1,64 @@
-package com.molano.formulariocomp;
+import java.io.Serializable;
+import java.util.Objects;
 
 /**
  *
  * @author Jhon
  */
-public class PersonaDTO {
-    
+public class PersonaDTO implements Serializable{
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String clave;
+
+    public PersonaDTO(String nombre, String apellido, String correo, String clave) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.clave = clave;
+    }
+
+    public PersonaDTO() {
+        this.nombre = "";
+        this.apellido = "";
+        this.correo = "";
+        this.clave = "";
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonaDTO{" + "nombre=" + nombre + ", apellido=" + apellido + ", correo=" + correo + ", clave=" + clave + '}';
+    }
 }
